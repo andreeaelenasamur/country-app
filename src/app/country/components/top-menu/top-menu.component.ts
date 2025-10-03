b7b7dc424fd8a90154121b801354f6657f15f96d
andreeaelenasamur <andelenasamur@gmail.com>
2025-10-03 10:06:52 +0200

    add top menu

---
+++ src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
+
+@Component({
+  selector: 'country-top-menu',
+  imports: [
+    RouterLink,
+    RouterLinkActive],
+  templateUrl: './top-menu.component.html',
+})
+export class TopMenuComponent { }
